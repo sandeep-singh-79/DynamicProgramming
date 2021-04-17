790686491a3cbb7f8f7cb7188615d68a05fd0147
Sandeep Singh <>
2021-04-17 16:56:52 +0000

    second implementation to calculate fix with tabulation DP

--- fibonacci/fibDPTabulation.py
+++ fibonacci/fibDPTabulation.py
@@ -9,3 +9,15 @@ def fibDPTabulation(position):
         table[i] = table[i-2] + table[i-1]
     
     return table[position]
+
+def fibDPTab(position):
+    table = list()
+    table = [0 for i in range(0, position+1)]
+    table[1] = 1
+
+    for index in range(position):
+        table[index+1] += table[index]
+        if index == position -1: break;
+        table[index+2] += table[index]
+    
+    return table[position]
--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,11 +1,12 @@
 from fibLoop import fibByLoop
 from fibRecursion import fibByRecursion
-from fibDPTabulation import fibDPTabulation
+import fibDPTabulation
 from fibonacciMemo import fibByRecursionMemo
 
 if __name__ == "__main__":
-    position = 0
+    position = 50
     print("Fibonacci number at position %d: %d" % (position, fibByLoop(position)))
     print("Fibonacci number at position %d: %d" % (position, fibByRecursionMemo(position)))
-    print("Fibonacci number at position %d: %d" % (position, fibDPTabulation(position)))
+    print("Fibonacci number at position %d: %d" % (position, fibDPTabulation.fibDPTabulation(position)))
+    print("Fibonacci number at position %d: %d" % (position, fibDPTabulation.fibDPTab(position)))
     print("Fibonacci number at position %d: %d" % (position, fibByRecursion(position)))
